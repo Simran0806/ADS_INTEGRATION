4664ef415f8cf94db5037948bc73190f2ee221fe
Simran0806 <127962890+Simran0806@users.noreply.github.com>
2023-03-31 00:37:18 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import "./style.css"
+
+function App() {
+
+
+
+
+  return (
+
+    <>
+
+
+      <header>
+        
+        <nav>
+          <h1>DUMMY</h1>
+          <ul>
+            <li>HOME</li>
+            <li>PRODUCTS</li>
+            <li>CONTACT</li>
+          </ul>
+        </nav>
+
+      </header>
+
+
+      <main>
+
+        <div>
+          content
+        </div>
+        <div>
+          content
+        </div>
+        <div>
+          content
+        </div>
+        <div>
+          content
+        </div>
+
+      </main>
+
+
+      <footer>
+        <div>dummy</div>
+        <div>dummy</div>
+        <div>dummy</div>
+        <div>dummy</div>
+        <div>dummy</div>
+        <div>dummy</div>
+      </footer>
+
+
+
+    </>
+  )
+}
+
+export default App
